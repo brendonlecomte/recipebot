f9f718ec50ae18418df8345616127f62450606ba
Brendon Le Comte <brendonlc@graphcore.ai>
2020-01-19 17:58:31 +0000

    comments if i come back to this project

--- calories.py
+++ calories.py
@@ -1,23 +1,27 @@
-#! /Users/brendonlc/research/recipbot/bin/python
 import requests
 from bs4 import BeautifulSoup
 headers = {'User-agent': 'Mozilla/5.0'}
 
+# adding the saved recipe links to this base gives the page
 basePath = 'https://www.bbcgoodfood.com{}'
 
+
 with open("mains-recipes.txt", "r") as f:
-    with open("nutrion.txt", "w") as out:
+    with open("nutrition.txt", "w") as out:
         i = 0
         for x in f.readlines():
             i += 1
             page = requests.get(basePath.format(x.strip()), headers=headers)
             soup = BeautifulSoup(page.content, 'html.parser')
 
+            # inspect element on the page. Find what you need specifically
             links = soup.find_all("span", "nutrition__value")
-            try:
+            try:  # try parse out calories, protein, and fat
                 calories = int(links[0].text)
                 protein = float(links[6].text[:-1])
                 fat = int(links[1].text[:-1])
+
+                # crude filter on requirements
                 if calories > 340 and calories < 560 and protein > 25 and fat < 20:
                     print(f"{i}: {basePath.format(x.strip())}")
                     out.write(f"{i}: {basePath.format(x.strip())}\n")
--- scrape.py
+++ scrape.py
@@ -1,19 +1,26 @@
-#! /Users/brendonlc/research/recipbot/bin/python
 import requests
 from bs4 import BeautifulSoup
+
+# Some websites dislike not having a header that looks like a web browser
 headers = {'User-agent': 'Mozilla/5.0'}
 
-# searchPath = 'https://www.bbcgoodfood.com/search/recipes?query=vegan&page={}'
+# carefully constructed search path allows us to look for the results we want
+# this could be changed for any website. Below is just for bbc good food
 searchPath = 'https://www.bbcgoodfood.com/search/recipes?query=&page={}'
 
 with open("mains-recipes.txt", "w") as f:
-    for x in range(0, 860):
+    for x in range(0, 860):  # 860 is the number of pages this result produces (at the time of commit)
+
+        # Generic request and parse
         page = requests.get(searchPath.format(x), headers=headers)
         soup = BeautifulSoup(page.content, 'html.parser')
 
+        # looked at the website and its returned data.
+        # This search gets me the objects I want. Recipe results
         links = soup.find_all("h3", "teaser-item__title", 'a href')
+
         for link in links:
-            print(str(link.a["href"]))
-            f.write(str(link.a["href"]))
+            print(str(link.a["href"]))  # print the returned recipe path
+            f.write(str(link.a["href"]))  #dump to file
             f.write("\n")
 
